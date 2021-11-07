b5066399cc251b09ccfa1e7c336180b505de22b5
ryichk <ryichk@gmail.com>
2021-11-07 17:19:28 +0900

    :wrench: Add typescript-eslint-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
-  extends: ['next', 'next/core-web-vitals'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:import/warnings',
+  ],
   rules: {
     semi: 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
     'import/order': [
       'error',
       {
