875ea0414ca86c24ae864e5fc6c1966501c4d06e
ryichk <ryichk@gmail.com>
2021-11-07 17:24:34 +0900

    :wrench: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SAMPLE: process.env.SAMPLE,
+  },
+  images: {
+    domains: [],
+  },
+  i18n: {
+    locales: ['en', 'ja'],
+    defaultLocale: 'ja',
+  },
 };
